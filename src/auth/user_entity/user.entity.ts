fc72e509aac3eedfb6d382bf5896629dd9e485c2
nzeo2008 <nzeo2008@gmail.com>
2022-12-25 09:32:29 +0700

    Created the user entity

---
+++ src/auth/user_entity/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	name: string;
+
+	@Column({ unique: true })
+	email: string;
+
+	@Column()
+	password: string;
+
+	@Column({ default: new Date() })
+	createdAt: Date;
+}
