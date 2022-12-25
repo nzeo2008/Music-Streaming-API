3e2dc0d363872fa475b8ab5188f9da263b564526
nzeo2008 <nzeo2008@gmail.com>
2022-12-25 09:31:49 +0700

    Added async connection to the db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getPostgresProperties } from './configs/database.config';
 
 @Module({
-	imports: [AuthModule],
+	imports: [
+		AuthModule,
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getPostgresProperties,
+		}),
+	],
+
 	controllers: [AppController],
 	providers: [AppService],
 })
